55b60cd63a617f7bdc1d19157d37ca6e5324f0a5
Tanya Chaturvedi <104621628+tanyaachaturvedi@users.noreply.github.com>
2025-05-17 17:41:32 +0530

    Time: 123 ms (5.48%) | Memory: 94.9 MB (5.22%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       int n=nums.size();
+       map<int,int>mpp;
+
+       for(int i=0; i<n;i++){
+        mpp[nums[i]]++;
+       } 
+
+       for(auto it:mpp){
+        if(it.second>1){
+            return true;
+        }
+       }
+       return false;
+    }
+};
