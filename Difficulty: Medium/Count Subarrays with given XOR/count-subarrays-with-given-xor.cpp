4d5f4221a6de96f2b96668f192b5f533505e3090
Tanya Chaturvedi <104621628+tanyaachaturvedi@users.noreply.github.com>
2025-06-06 20:44:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -1,54 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     long subarrayXor(vector<int> &arr, int k) {
         // code here
-        int n=arr.size();
-        long x=0,res=0;
-        unordered_map<int,int> map;
-        for(int i=0;i<n;i++)
-        {
-            x=x^arr[i];
-            res+=map[x^k];
-            if(x==k) res++;
-            map[x]++;
+        int xr=0, cnt=0;
+        map<int,int> mpp;
+        mpp[xr]++;
+        
+        for(int i=0; i<arr.size(); i++){
+            xr= xr^arr[i];
+            
+            int x= xr^k;
+            cnt += mpp[x];
+            mpp[xr]++;
         }
-        return res;
+        return cnt;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore(); // Ignore the newline after the number of test cases
-
-    while (tc-- > 0) {
-        string input;
-        getline(cin, input); // Read the whole line for the array
-
-        stringstream ss(input);
-        vector<int> arr;
-        int num;
-
-        while (ss >> num) {
-            arr.push_back(num); // Push numbers into the vector
-        }
-
-        int k;
-        cin >> k;
-        cin.ignore(); // Ignore the newline after k
-
-        Solution obj;
-        cout << obj.subarrayXor(arr, k) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
