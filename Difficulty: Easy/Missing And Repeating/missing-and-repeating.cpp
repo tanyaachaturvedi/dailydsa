1f7dfa2e1ee2f0818a1f9d02fc5c72780ed916e3
Tanya Chaturvedi <104621628+tanyaachaturvedi@users.noreply.github.com>
2025-06-19 04:02:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        long long n=arr.size();
+        
+        long long SN=(n*(n+1))/2;
+        long long S2N=(n*(n+1)*(2*n+1))/6;
+        long long S=0,S2=0;
+        
+        for(int i=0;i<n;i++){
+            S += arr[i];
+            S2 += (long long)arr[i] * (long long)arr[i];
+        }
+        
+        long long val1=S-SN;
+        long long val2= S2-S2N;
+        
+        val2=val2/val1;
+        
+        long long x=(val1+val2)/2;
+        long long y=x-val1;
+        
+        return {(int)x, (int)y};
+    }
+};
